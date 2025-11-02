019620ba345a897daba974c6ebd64020f0b14fba
ashan012 <khan89891918@outlook.com>
2025-11-02 14:31:46 +0500

    change in utils

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+import conncectionDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config({ path: "./.env" });
+
+const Port = process.env.PORT || 8000;
+
+conncectionDB()
+  .then(() => {
+    app.listen(Port, () => {
+      console.log(`server listen on Port ${Port}`);
+    });
+  })
+  .catch((error) => console.log(`error in MONGO CONNECTION ${error}`));
